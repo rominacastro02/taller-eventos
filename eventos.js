6ff576d88e9ac428a8dbb33968bf00c1bf9de980
rominacastro02 <rominica02@gmail.com>
2024-08-26 11:24:50 -0300

    Solución al problema de propagación del click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
-document.getElementById('contenedor-boton').addEventListener('click', function() {
+document.querySelector('button').addEventListener('click', function(event) {
+    alert('Hola!');
+    event.stopPropagation(); // Detiene la propagación del evento al div padre
+});
+
+document.querySelector('div').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
